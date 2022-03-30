c8c4db6cfa2e54099d3c9c52039a0783fb7f1cfa
Kvnsky <violentfx1@gmail.com>
2022-03-30 21:54:21 +0200

    Refactor scraper

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+const writeFileSync = require('fs').writeFileSync;
 const puppeteer = require('puppeteer');
-const fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -7,47 +7,18 @@ const fs = require('fs');
   await page.goto('https://kog.tw/#p=maps', {
     waitUntil: 'networkidle2',
   });
-  await page.screenshot({ path: 'test.png' });
 
-  const mapNames = await page.evaluate(() => {
-    const mapNames = [];
-    const mapNameElements = document.querySelectorAll(
-      '.my-0, .font-weight-normal'
-    );
-    for (let i = 0; i < mapNameElements.length; i++) {
-      mapNames.push(mapNameElements[i].innerText);
-    }
-
-    return mapNames;
-  });
-
-  const mapPoints = await page.evaluate(() => {
-    const mapPoints = [];
-    const mapPointElements = document.querySelectorAll(
-      '.list-group-item:nth-child(3)'
-    );
-    for (let i = 0; i < mapPointElements.length; i++) {
-      mapPoints.push(mapPointElements[i].innerText);
-    }
-
-    return mapPoints;
-  });
-
-  const mapDifficulties = await page.evaluate(() => {
-    const mapDifficulties = [];
-    const mapDifficultyElements = document.querySelectorAll(
-      '.list-group-item:nth-child(2)'
-    );
-    for (let i = 0; i < mapDifficultyElements.length; i++) {
-      mapDifficulties.push(mapDifficultyElements[i].innerText);
-    }
-
-    return mapDifficulties;
-  });
-
-  fs.writeFileSync('mapNames.json', JSON.stringify(mapNames));
-  fs.writeFileSync('mapPoints.json', JSON.stringify(mapPoints));
-  fs.writeFileSync('mapDifficulties.json', JSON.stringify(mapDifficulties));
-
+  const maps = await page.evaluate(() =>
+    Array.from(document.querySelectorAll('.col')).map((map) => [
+      map.querySelector('.my-0, .font-weight-normal').innerText,
+      map.querySelector('.list-group-item:nth-child(2)').innerText,
+      map.querySelector('.list-group-item:nth-child(3)').innerText,
+    ])
+  );
+  writeToJson(maps);
   await browser.close();
 })();
+
+const writeToJson = (maps) => {
+  writeFileSync('./maps.json', JSON.stringify(maps));
+};
