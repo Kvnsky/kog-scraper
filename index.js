65c58a6b98f538e6f167bd93f4dcf04fa201cfaa
Kvnsky <violentfx1@gmail.com>
2022-04-04 00:09:39 +0200

    Actually format json this time

--- index.js
+++ index.js
@@ -8,13 +8,24 @@ const puppeteer = require('puppeteer');
     waitUntil: 'networkidle2',
   });
 
-  const maps = await page.evaluate(() =>
+  let maps = await page.evaluate(() =>
     Array.from(document.querySelectorAll('.col')).map((map) => [
       map.querySelector('.my-0, .font-weight-normal').innerText,
       map.querySelector('.list-group-item:nth-child(2)').innerText,
       map.querySelector('.list-group-item:nth-child(3)').innerText,
     ])
   );
+
+  maps = maps.map((mapArray) => {
+    return {
+      map: mapArray[0],
+      difficulty: mapArray[1],
+      points: parseInt(mapArray[2].split(' ')[0]),
+    };
+  });
+
+  maps.sort((a, b) => a.points - b.points);
+
   writeToJson(maps);
   await browser.close();
 })();
